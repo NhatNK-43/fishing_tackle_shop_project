44821b9a9a7fb99b0013046036ad504f6f039f29
NhatNK-43 <khanh.nhat.365@gmail.com>
2024-01-04 07:34:27 +0700

    commit to push

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HomePage} from "./components/HomePage";
+import {Cart} from "./components/Cart";
+import {ProductDetail} from "./components/ProductDetail";
+import {LoginAndRegister} from "./components/LoginAndRegister";
+import {ToastContainer} from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/hompage" element={<HomePage/>}></Route>
+          <Route path="/login" element={<LoginAndRegister/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+          <Route path="/product-detail" element={<ProductDetail/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer/>
+    </>
   );
 }
 
