0c1fc1d139b4ef71be41f3147c80606330fe4844
NhatNK-43 <khanh.nhat.365@gmail.com>
2024-01-04 07:34:41 +0700

    commit to push

---
+++ front_end/src/components/ProductDetail.js
@@ -0,0 +1,205 @@
+export function ProductDetail(){
+
+    return(
+        <>
+            <section className="py-5">
+                <div className="container">
+                    <div className="row gx-5">
+                        <aside className="col-lg-6">
+                            <div className="border rounded-4 mb-3 d-flex justify-content-center">
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="rounded-4"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/fb17ed6401faa52538de9db8bd1ccc07"
+                                >
+                                    <img
+                                        style={{ maxWidth: "100%", maxHeight: "100vh", margin: "auto" }}
+                                        className="rounded-4 fit"
+                                        src="https://down-vn.img.susercontent.com/file/fb17ed6401faa52538de9db8bd1ccc07"
+                                    />
+                                </a>
+                            </div>
+                            <div className="d-flex justify-content-between mb-3">
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="border mx-1 rounded-2 w-25"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/cc300da7ad4e4951ae168a9d3551ffe3"
+                                >
+                                    <img
+                                        width="100%"
+                                        height="100%"
+                                        className="rounded-2"
+                                        src="https://down-vn.img.susercontent.com/file/cc300da7ad4e4951ae168a9d3551ffe3"
+                                    />
+                                </a>
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="border mx-1 rounded-2 w-25"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/3112e081d045b19ff3bc192503e3520a"
+                                >
+                                    <img
+                                        width="100%"
+                                        height="100%"
+                                        className="rounded-2"
+                                        src="https://down-vn.img.susercontent.com/file/3112e081d045b19ff3bc192503e3520a"
+                                    />
+                                </a>
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="border mx-1 rounded-2 w-25"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/88c08d6860c929aa4f8faa7c3ca2db30"
+                                >
+                                    <img
+                                        width="100%"
+                                        height="100%"
+                                        className="rounded-2"
+                                        src="https://down-vn.img.susercontent.com/file/88c08d6860c929aa4f8faa7c3ca2db30"
+                                    />
+                                </a>
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="border mx-1 rounded-2 w-25"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/e31511573424b3b0730bdc59be8581c3"
+                                >
+                                    <img
+                                        width="100%"
+                                        height="100%"
+                                        className="rounded-2"
+                                        src="https://down-vn.img.susercontent.com/file/e31511573424b3b0730bdc59be8581c3"
+                                    />
+                                </a>
+                                <a
+                                    data-fslightbox="mygalley"
+                                    className="border mx-1 rounded-2 w-25"
+                                    target="_blank"
+                                    data-type="image"
+                                    href="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhdtesclxicy40"
+                                >
+                                    <img
+                                        width="100%"
+                                        height="100%"
+                                        className="rounded-2"
+                                        src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lhdtesclxicy40"
+                                    />
+                                </a>
+                            </div>
+                            {/* thumbs-wrap.// */}
+                            {/* gallery-wrap .end// */}
+                        </aside>
+                        <main className="col-lg-6">
+                            <div className="ps-lg-3">
+                                <h4 className="title text-dark">
+                                    Cần Câu Tay SHIMANO Cacbon Cao Cấp - GIÁ SIÊU RẺ do Huandocau Phân
+                                    Phối Chính Hãng Đủ size 2m7-3m6-4m5-5m4-6m3 test
+                                </h4>
+                                <div className="d-flex flex-row my-3">
+                                    <div className="text-warning mb-1 me-2">
+                                        <i className="fa fa-star" />
+                                        <i className="fa fa-star" />
+                                        <i className="fa fa-star" />
+                                        <i className="fa fa-star" />
+                                        <i className="fas fa-star-half-alt" />
+                                        <span className="ms-1">4.5</span>
+                                    </div>
+                                    <span className="text-muted">
+              <i className="fas fa-shopping-basket fa-sm mx-1" />
+              154 orders
+            </span>
+                                    <span className="text-success ms-2">In stock</span>
+                                </div>
+                                <div className="mb-3">
+                                    <span className="h5">$75.00</span>
+                                    <span className="text-muted">/per box</span>
+                                </div>
+                                <p>
+                                    Huandocau Là Đại Lý Phân Phối Đồ Câu Tốt Nhất Trên Thị Trường Hiện
+                                    Tại.
+                                    <br />
+                                    -Chúng Tôi Chuyên Cung Cấp Các Loại Đồ Câu, Ngư Cụ Với Đầy Đủ Mặt
+                                    Hàng Và Giá Cả Tốt Nhất Đến Khách Hàng.
+                                    <br />
+                                    -Với Đội Ngũ Chăm Sóc Khách Hàng, Tư Vấn Nhiệt Tình.
+                                    <br />
+                                    -Chúng Tôi Tự Tin Sẽ Mang Lại Trải Nghiệm Mua Hàng Tốt Nhất Đến Mọi
+                                    Khách Hàng!
+                                    <br />
+                                </p>
+                                <div className="row">
+                                    <dt className="col-3">Loại:</dt>
+                                    <dd className="col-9">Cần câu tay</dd>
+                                    <dt className="col-3">Màu sắc:</dt>
+                                    <dd className="col-9">Đen</dd>
+                                    <dt className="col-3">Chất liệu:</dt>
+                                    <dd className="col-9">Cacbon</dd>
+                                    <dt className="col-3">Xuất xứ:</dt>
+                                    <dd className="col-9">Nhật Bản</dd>
+                                </div>
+                                <hr />
+                                <div className="row mb-4">
+                                    <div className="col-md-4 col-6">
+                                        <label className="mb-2">Kích thước</label>
+                                        <select
+                                            className="form-select border border-secondary"
+                                            style={{ height: 35 }}
+                                        >
+                                            <option>2M7</option>
+                                            <option>3M6</option>
+                                            <option>4M5</option>
+                                        </select>
+                                    </div>
+                                    {/* col.// */}
+                                    <div className="col-md-4 col-6 mb-3">
+                                        <label className="mb-2 d-block">Số lượng</label>
+                                        <div className="input-group mb-3" style={{ width: 170 }}>
+                                            <button
+                                                className="btn btn-white border border-secondary px-3"
+                                                type="button"
+                                                id="button-addon1"
+                                                data-mdb-ripple-color="dark"
+                                            >
+                                                <i className="fas fa-minus" />
+                                            </button>
+                                            <input
+                                                type="text"
+                                                className="form-control text-center border border-secondary"
+                                                placeholder={1}
+                                                aria-label="Example text with button addon"
+                                                aria-describedby="button-addon1"
+                                            />
+                                            <button
+                                                className="btn btn-white border border-secondary px-3"
+                                                type="button"
+                                                id="button-addon2"
+                                                data-mdb-ripple-color="dark"
+                                            >
+                                                <i className="fas fa-plus" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" className="btn btn-warning shadow-0">
+                                    {" "}
+                                    Mua ngay{" "}
+                                </a>
+                                <a href="#" className="btn btn-primary shadow-0">
+                                    {" "}
+                                    <i className="me-1 fa fa-shopping-basket" /> Thêm vào giỏ hàng{" "}
+                                </a>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
